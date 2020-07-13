740052929db9f4b083b6c14180c081db002ca0d5
MarvinMonnar <monnarmarvin@gmail.com>
2020-07-13 03:35:18 -0600

    subiendo condicion if

---
+++ Ejercicios/Ejercicio 9/condicionesif.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numero = 0;
+    int numeroSecreto = 7 ;
+    cout<< "Ingrese un numero: " ;
+    cin >> numero;
+
+    cout<< endl;
+
+    if (numero = numeroSecreto)
+    {
+        cout<< "Adivinaste el numero secreto" << numero;
+    } else
+    {
+    cout<< "No adivinaste el numero secreto" << numero;
+
+    }
+    
+    
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Ingresaste el numero: " << numero;
+
+
+    return 0;
+}
