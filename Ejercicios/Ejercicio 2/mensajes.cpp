cbe96c638189395ee65f3c4dc1723fc90e82da48
MarvinMonnar <monnarmarvin@gmail.com>
2020-07-13 02:49:53 -0600

    añadiendo

---
+++ Ejercicios/Ejercicio 2/mensajes.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string Nombre;
+
+    cout<< "Ingrese su nombre: ";
+    cin>> Nombre;
+    
+    cout<< endl;
+    
+    cout<< "Mi nombre es:" << Nombre;
+
+    cout<< endl;
+    return 0;
+}
+ 
---
+++ Ejercicios/Ejercicios/Tarea 1/tarea1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+int a = 0;
+ int b = 0;
+ int resultado = 0;
+ int resultado2 = 0;
+ int resultado3 = 0;
+ int resultado4 = 0;
+ 
+ cout<< "Ingrese el valor de a: " ;
+ cin >> a;
+ 
+ cout<< endl;
+ 
+ cout<< "Ingrese el valor de b: ";
+ cin>> b; 
+ 	
+
+ cout<< endl;
+ 
+ resultado = a+b;
+ resultado2 = a-b;
+ resultado3 = a*b;
+ resultado4 = a/b;
+ 
+ cout << "El resultado de a + b es:" << resultado;
+ cout << endl;
+cout << "El resultado de a - b es:" << resultado2;
+cout << endl;
+cout << "El resultado de a * b es:" << resultado3;
+cout << endl;
+cout << "El resultado de a / b es:" << resultado4;
+cout << endl;
+    return 0;
+}
