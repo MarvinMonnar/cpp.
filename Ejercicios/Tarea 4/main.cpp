5cc11e106b499f2ab9f0788cff5ad2ce59a953a8
MarvinMonnar <monnarmarvin@gmail.com>
2020-07-15 01:48:15 -0600

    Tarea 4

---
+++ Ejercicios/Tarea 4/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+	using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int pares = 0;
+	int impares = 0;
+	int total = 0;
+	
+	
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i % 2 == 0)
+		{
+		
+			pares = pares + i;
+						
+		}
+		
+		else 
+		
+			impares = impares + i;
+			
+		}			
+		
+	}
+	
+
+	total = pares + impares ;
+	
+	
+	
+	cout << endl;
+	cout << endl;
+	cout << "Total pares: " << pares << endl;
+	cout << "Total impares: " << impares << endl;
+	cout <<"Total: " << total << endl;
+	
+
+	
+	
+	
+	
+	return 0;
+}
