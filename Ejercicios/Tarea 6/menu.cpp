04b243f37758680010ecf688875793d89c7b89d2
MarvinMonnar <monnarmarvin@gmail.com>
2020-07-28 17:31:33 -0600

    Tarea 6

---
+++ Ejercicios/Tarea 6/menu.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+extern void productos(int opcion);
+extern void imprimirFactura();
+
+void menu()
+{
+
+int opcion = 0;
+
+while (true)
+	{
+		system("cls");
+	
+		cout << "**********";
+		cout << "MENU";
+		cout << "**********";
+		cout << endl;
+		cout << endl;
+		cout << endl;
+		
+		cout << "1 - BEBIDAS CALIENTES"<<endl;
+		cout << "2 - BEBIDAS FRIAS"<<endl;
+		cout << "3 - REPOSTERIA"<<endl;
+		cout << "4 - IMPRIMIR FACTURA"<<endl;
+		cout << "0 - SALIR"<<endl;
+		
+		cout << endl;
+		cout << "INGRESE UNA OPCION: ";
+		cin >> opcion;
+		
+		if (opcion == 0)
+		{
+			cout<<endl;
+			cout <<"*****GRACIAS POR VISITARNOS,VUELVA PRONTO*****" << endl;
+			break;
+		}
+		
+		if (opcion == 4)
+		
+		{
+			imprimirFactura(); 
+			cout << endl;
+			cout <<"*****GRACIAS POR VISITARNOS,VUELVA PRONTO*****" << endl;
+			cout << endl;
+			system ("pause");
+		}
+		
+		else
+		{
+			productos(opcion);
+
+		}
+		
+	}
+}
